0ca7d7727e488ec8f8becf8e824d0b72daea5313
jamesAkash <akash.james@outlook.com>
2024-10-26 12:22:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-slate-400 h-[100vh]">
+      <h1 className="text-4xl bg-yellow-400">Test</h1>
+    </div>
+  );
+}
+
+export default App;
